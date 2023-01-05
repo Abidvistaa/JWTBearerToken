3e484867fc3adacf80eec68be313db65d1be414e
Abid Hassan <abidhassandiba@gmail.com>
2023-01-05 15:43:40 +0600

    Add project files.

---
+++ TokenJWT/Controllers/AuthenticationController.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using TokenJWT.Authentication;
+
+namespace TokenJWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AuthenticationController( IConfiguration configuration )
+        {
+            _configuration = configuration;
+        }
+        #region Test With Database
+        //[HttpPost]
+        //[Route("Register")]
+        //public async Task<IActionResult> Register([FromBody] RegisterModel model)
+        //{
+        //    var userExist = await userManager.FindByNameAsync(model.UserName);
+        //    if (userExist != null)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User Name Already Exists" });
+        //    }
+        //    ApplicationUser user = new ApplicationUser()
+        //    {
+        //        Email = model.Email,
+        //        SecurityStamp = Guid.NewGuid().ToString(),
+        //        UserName = model.UserName
+        //    };
+
+        //    var result = await userManager.CreateAsync(user, model.Password);
+        //    if (!result.Succeeded)
+        //    {
+        //        return StatusCode(StatusCodes.Status500InternalServerError, new Response { Status = "Error", Message = "User Creation Falied" });
+        //    }
+        //    return Ok(new Response { Status = "Success", Message = "User Creation Successfull" });
+        //}
+
+        //[HttpPost]
+        //[Route("Login")]
+        //public async Task<IActionResult> Login([FromBody] LoginModel model)
+        //{
+        //    var user = await userManager.FindByIdAsync(model.UserName);
+        //    if(user!=null && await userManager.CheckPasswordAsync(user, model.Password))
+        //    {
+        //        var userRoles = await userManager.GetRolesAsync(user);
+        //        var authClaims = new List<Claim>
+        //        {
+        //            new Claim(ClaimTypes.Name,user.UserName),
+        //            new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+        //        };
+        //        foreach(var userrole in userRoles)
+        //        {
+        //            authClaims.Add(new Claim(ClaimTypes.Role, userrole));
+        //        }
+        //        var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+        //        var token = new JwtSecurityToken(
+        //            issuer: _configuration["JWT:ValidIssuer"],
+        //            audience: _configuration["JWT:ValidAudience"],
+        //            expires: DateTime.Now.AddHours(5),
+        //            claims: authClaims,
+        //            signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+        //            );
+        //        return Ok(new
+        //        {
+        //            token = new JwtSecurityTokenHandler().WriteToken(token),
+        //        });
+        //    }
+        //    return Unauthorized();
+        //}
+        #endregion
+
+        #region Without Database
+        [HttpPost]
+        [Route("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel model)
+        {
+            if (model.UserName == "Abid" && model.Password == "123")
+            {
+                var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name,model.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti,Guid.NewGuid().ToString())
+                };
+
+                var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+                var token = new JwtSecurityToken(
+                    issuer: _configuration["JWT:ValidIssuer"],
+                    audience: _configuration["JWT:ValidAudience"],
+                    expires: DateTime.Now.AddHours(5),
+                    claims: authClaims,
+                    signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                    );
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+            return Unauthorized();
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("GetAPI")]
+        public String GetAPI()
+        {
+            return "Success!!! Now You Can Get The API";
+        }
+        #endregion
+
+    }
+}
